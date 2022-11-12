8aadd63b44be197714a1b90082f8a3e1a94275d2
Antonio Robles <antjrobles@gmail.com>
2022-11-12 10:57:19 +0100

    fourth commit ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Lets Get Started!!! ✅
           </Link>
         </div>
       </div>
