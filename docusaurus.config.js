607ebd9434e7ccbf6d8329922e521c35c24d5aad
Antonio Robles <51507400+Antjrobles@users.noreply.github.com>
2023-03-29 10:15:10 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'My Second Brain',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
