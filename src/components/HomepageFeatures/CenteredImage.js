038ee3256ebc56c11b4bc7c8c0dfff985ba2db5f
Antonio Robles <antjrobles@gmail.com>
2024-07-12 15:44:25 +0200

    db01 - acceso

--- src/components/HomepageFeatures/CenteredImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CenteredImage = ({ src, alt, width, height }) => (
-  <div style={{ textAlign: 'center' }}>
-    <img src={src} alt={alt} style={{ width, height }} />
-  </div>
-);
-
-export default CenteredImage;
