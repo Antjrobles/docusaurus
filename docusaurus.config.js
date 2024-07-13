7a7fe3e2db4673f4ce7dd9112cccbdd062b37700
Antonio Robles <antjrobles@gmail.com>
2024-07-13 19:47:43 +0200

    powershell syntax

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,7 +100,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['powershell', 'shell'],
+        additionalLanguages: ['powershell'],
       },
     }),
   customFields: {
